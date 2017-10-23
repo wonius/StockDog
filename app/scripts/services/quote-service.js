0dcb9f48046fbd9db06b0e3cd1a680181038b28e
Gavin <woniuyf@163.com>
2017-10-23 22:11:44 +0800

    1.6集成Yahoo Finance BugFix：修复yql查询失败

--- app/scripts/services/quote-service.js
+++ app/scripts/services/quote-service.js
@@ -49,9 +49,6 @@ angular.module('stockDogApp')
         return symbols;
       }, []);
 
-
-      //https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22YHOO%22%2C%22AAPL%22%2C%22GOOG%22%2C%22MSFT%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=
-      //https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20('')&format=json&diagnostics=true&env=store://datatables.org/alltableswithkeys"
       // var query = encodeURIComponent('select * from yahoo.finance.quotes ' +
       //   'where symbol in (\'' + symbols.join('\',\'') + '\')');
       // var url = BASE + '?' + 'q=' + query + '&format=json&diagnostics=true' +
